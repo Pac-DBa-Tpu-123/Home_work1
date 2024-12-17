f022a06d37e53ebb258d1abddf8555212b424c05
Egor <timofeev_ea05@list.ru>
2024-12-17 23:37:00 +0400

    first commit

---
+++ src/main/java/main1/hw1/Task_1.java
@@ -0,0 +1,29 @@
+package main1.hw1;
+
+public class Task_1 {
+
+    public static int minutesToSeconds(String time) {
+        if (time == null || !time.matches("\\d{1,}:\\d{2}")) {
+            return -1;
+        }
+
+        String[] parts = time.split(":");
+        int minutes = Integer.parseInt(parts[0]);
+        int seconds = Integer.parseInt(parts[1]);
+
+        if (seconds >= 60) {
+            return -1;
+        }
+
+        return minutes * 60 + seconds;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minutesToSeconds("01:00"));
+        System.out.println(minutesToSeconds("13:56"));
+        System.out.println(minutesToSeconds("10:60"));
+        System.out.println(minutesToSeconds("999:59"));
+        System.out.println(minutesToSeconds("12:ab"));
+        System.out.println(minutesToSeconds("12:09:10"));
+    }
+}
