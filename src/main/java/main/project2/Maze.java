933c8deb0865975b28f29974fead400d619ab4a3
Egor <timofeev_ea05@list.ru>
2025-03-10 20:41:45 +0400

    Fourth homework, the first check

--- src/main/java/main/project2/Maze.java
+++ src/main/java/main/project2/Maze.java
@@ -3,7 +3,6 @@ package main.project2;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Maze extends JPanel implements Runnable {
